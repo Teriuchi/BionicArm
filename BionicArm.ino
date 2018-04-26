99caa7a0a52a8371dbfa800e9885034127fece25
Striget <32745570+Striget@users.noreply.github.com>
2018-04-26 12:49:22 +0300

    v1.0

--- BionicArm.ino
+++ BionicArm.ino
@@ -1,34 +1,56 @@
-// Miika Laeslehto & Jaakko Palomäki
+// Miika Laeslehto & Jaakko Palomäki TTV17S1 Kajaani University of Applied Science
 
-#include <Servo.h>                // Servo library
-Servo finger;                     // Määritetään servo
-int flex = 0;                     // Onko sormi koukistettu
-int raja = 20;                    // EMG rajan ylitys aktivoimaan servo
-unsigned long edellinenLiike = 0; // Säilyttää milloin viimeksi servo liikkunut
-const long viive = 3000;       // viive
+#include <Servo.h>            // Servo library
+Servo thumb;                  // Thumb finger servo
+Servo index;                  // Index finger servo
+Servo middle;                 // Middle finger servo
+Servo ring;                   // Ring finger servo
+Servo little;                 // Little finger servo 
+bool flex = 0;                // Status for fingers
+int border = 20;              // EMG signal border to activate servos
+unsigned long lastFlex = 0;   // Last time servos moved
+const long latency = 1500;    // latency for fingers' movement stability
 
 void setup() {
-  finger.attach(3);   //Servo
-  pinMode(A0, INPUT); //EMG
-  Serial.begin(9600); // Serial monitor
-  finger.write(0);    //Nollataan servo
-  
+  thumb.attach(3);            // Define servos
+  index.attach(5);  
+  middle.attach(6); 
+  ring.attach(9);   
+  little.attach(10); 
+  pinMode(A0, INPUT);         // EMG
+  Serial.begin(9600);         // Serial monitor
+  thumb.write(0);             // Reset Servos
+  index.write(10);    
+  middle.write(0);   
+  ring.write(120);     
+  little.write(175);   
 }
 
 void loop() {
-  int emg = analogRead(A0); // lukee myosignaalin
-  unsigned long kulunutAika = millis(); // muuttuva tila
+  int emg = analogRead(A0);   // Read myosignal from muscle
+  unsigned long currentTime = millis(); // Changing current time
+
+  if(emg > border && flex == 0 && currentTime - lastFlex >= latency){
+      // If EMG-signal is higher than borderline and movement latency has been passed -> move fingers 
+      thumb.write(90);        // Turn servos to close fingers
+      index.write(85);    
+      middle.write(120);   
+      ring.write(5);     
+      little.write(50);   
+      flex = 1;               // Update status for closed fingers
+      lastFlex = millis();    // Register the last fingers' movement time
+   }
+   
+   else if(emg < border && flex == 1 && currentTime - lastFlex >= latency) {
+      thumb.write(0);         // Turn servos to open fingers
+      index.write(10);    
+      middle.write(0);   
+      ring.write(120);     
+      little.write(175);   
+      flex = 0;               // Update status for open fingers
+      lastFlex = millis();    // Register the last fingers' movement time
+   }
+  Serial.println(analogRead(A0));  // Print emg status
   
-  if(emg > raja && flex == 0 && kulunutAika - edellinenLiike >= viive){
-    finger.write(180);  
-    flex = 1;
-  }    
-  else if(emg > raja && flex == 1 && kulunutAika - edellinenLiike >= viive){
-    finger.write(0);
-    flex = 0;
-  }
-  
-  Serial.println(analogRead(A0));  // Tulostaa EMG-signaalin arvon
-  
-  delay(10);  // Viive jotta stabiili
+  delay(10);                  // Delay for stability
 }
